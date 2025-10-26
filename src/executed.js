aa00a66a17c522822230d684e1d0eacd3d1139e9
Leroiduxd <89317997+Leroiduxd@users.noreply.github.com>
2025-10-26 23:38:21 +0100

    Update executed.js

--- src/executed.js
+++ src/executed.js
@@ -14,9 +14,7 @@ async function main() {
 
   contract.on('Executed', async (id, entryX6, evt) => {
     try {
-      // adapte au nouveau db.js: un seul param { id, entryX6 }
       await handleExecutedEvent({ id, entryX6 });
-
       logInfo(
         TAG,
         `stored id=${id?.toString?.() ?? id} entryX6=${entryX6?.toString?.() ?? entryX6} @ block=${evt.blockNumber} tx=${evt.transactionHash} logIndex=${evt.logIndex}`
@@ -27,11 +25,7 @@ async function main() {
   });
 }
 
-// garde-fous
 process.on('unhandledRejection', (err) => logErr(TAG, 'unhandledRejection', err));
 process.on('uncaughtException', (err) => logErr(TAG, 'uncaughtException', err));
 
-main().catch((e) => {
-  logErr(TAG, e);
-  process.exit(1);
-});
+main().catch((e) => { logErr(TAG, e); process.exit(1); });
